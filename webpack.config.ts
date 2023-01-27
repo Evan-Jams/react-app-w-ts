21a669bebd1ff6e94acd2c6c0effa2e717bfd750
Evan James <thevan777@gmail.com>
2023-01-27 17:52:13 -0500

    created base of react app with typescript w/o using cra

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+import { Configuration } from "webpack";
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config = (): Configuration => {
+    return {
+        mode: 'production', // could be development and production
+        entry: path.resolve(__dirname, "./src/index.tsx"),
+        output: {
+            path: path.resolve(__dirname, "./dist/"),
+            filename: "[name}.js",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/i,
+                    exclude: [/node_modules/, /\.(spec|test).(ts|js)x?$/i],
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                        cacheCompression: false,
+                        compact: true,
+                    },
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
+            alias: {
+                src: path.resolve(__dirname, "./src/"),
+            },
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "index.html",
+                inject: true,
+            }),
+        ],
+    };
+};
+
+export default config;
